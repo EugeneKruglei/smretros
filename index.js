5e847b414b7d42acfc81bdf4eec279e7f9e6d14b
EugeneKruglei <35628216+EugeneKruglei@users.noreply.github.com>
2018-10-10 18:41:38 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 class Sorter {
-  constructor() {
-      this.elem = [];
-      this.reverse = (left, right) => right - left;
-  }
 
-  add(element) {
-      this.elem.push(element);
-  }
+    constructor() {
+        this.elem = [];
+        this.compFunc = function(a, b) {
+            return a - b
+        };
+    }
 
-  at(index) {
-      return this.elem[index];
-  }
+    add(element) {
+        this.elem.push(element);
+    }
 
-  get length() {
-      return this.elem.length;
-  }
+    at(index) {
+        return this.elem[index];
+    }
 
-  toArray() {
-      return this.elem;
-  }
+    get length() {
+        return this.elem.length;
+    }
 
-  sort(indices) {
-      let arr = [];
+    toArray() {
+        return this.elem;
+    }
 
-      for ( let i in indices ) {
-          arr.push( this.elem[ indices[i] ] );
-      }
+    sort(indices) {
+        let arrayOfElem = this.elem;
+        let sortVar = [];
+        indices.sort();
+        indices.forEach(function (element) {
+            sortVar.push(arrayOfElem[element])
+        });
+        sortVar.sort(this.compFunc);
+        let alpha = 0;
+        indices.forEach(function (element) {
+            arrayOfElem[element] = sortVar[alpha];
+            alpha++
+        });
+        this.elem = arrayOfElem;
+    }
 
-      arr.sort( this.reverse );
-      indices.sort( this.reverse );
-
-      let number = 0;
-      for ( const y in indices ) {
-          this.elem[indices[y]] = arr[number];
-          number++;
-      }
-
-  }
-
-  setComparator(compareFunction) {
-      this.reverse = ;
-  }
+    setComparator(compareFunction) {
+        this.compFunc = compareFunction;
+    }
 }
 
 module.exports = Sorter;
